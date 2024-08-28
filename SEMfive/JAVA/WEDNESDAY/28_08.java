05ee1adfc10cc2788dcf03c58d519b26db65e004
Manasvi8274 <Manasvi1238274@gmail.com>
2024-08-28 21:16:45 +0530

    commit

--- SEMfive/JAVA/WEDNESDAY/28_08.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-package SEMfive.JAVA.WEDNESDAY;
-
-public class 28_08{
-    
-}
---
+++ SEMfive/JAVA/WEDNESDAY/Aug_28.java
@@ -0,0 +1,9 @@
+package semfive.JAVA.WEDNESDAY;
+import java.util.*;
+
+public class Aug_28{
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("123"); 
+    }
+}

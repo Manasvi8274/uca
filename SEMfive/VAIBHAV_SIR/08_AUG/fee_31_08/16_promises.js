cf8bd3b5a9e4b0933dc5237e80ece190ddddb0df
Manasvi8274 <Manasvi1238274@gmail.com>
2024-08-31 12:12:34 +0530

    commit

--- SEMfive/VAIBHAV_SIR/08_AUG/fee_31_08/16_promises.js
+++ SEMfive/VAIBHAV_SIR/08_AUG/fee_31_08/16_promises.js
@@ -1,1 +1,103 @@
 //chaining + this keyword  = promise
+//formal object 
+//created using constructor function - most usefull,extensive,best
+// object.create = create a new object , object.assign = an object is reference with another object
+//promise is an objeect created using promise() constructor funcion  and have 2 properties and have two properties and have -
+// 1.state
+// 2.value
+// State - possible values:
+//  - initial state = pending - waiting
+//  - fulfilled - sucess
+//  - rejected - reject
+// Value - possible values:
+//  - initial = undefined(does not depend on anyone)
+//  return value or error
+
+////////////////////////////////////basic promise - SAMPLE Promise()
+// function promiseExecutor() {
+//     setTimeout(() => {
+//         console.log("inside promiseExecutor");
+//     }, 10000);
+// }
+
+// var promise1 = new Promise(promiseExecutor)//normal way,create a simple promise
+
+//to find which constructor function is used
+// console.log(promise1.constructor.name)
+// console.log(promise1.name)//not defined
+// console.log(promise1.constructor)
+
+// console.log(promise1)
+
+
+//-----------------------------------fullfiled promise
+// function promiseExecutor(resolve, reject) {
+//     resolve({ response: "resolve promise" });
+//     console.log("inside promiseExecutor");
+// }
+
+// var promise1 = new Promise(promiseExecutor)
+// console.log(promise1);//only get out of pending state if any one parameter get called
+
+
+///----------------------------------------rejected promise - always give an uncautgh a promise error
+// function promiseExecutor(resolve, reject) {
+//     reject({ response: "reject promise" });
+//     console.log("inside promiseExecutor");
+// }
+
+// var promise1 = new Promise(promiseExecutor)
+// console.log(promise1);//only get out of pending state if any one parameter get called
+
+//chain methods  -  then and catch----------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// let responsevalue;
+
+// function promiseExecutor(resolve, reject) {
+//     resolve({ response: "resolve promise" });
+//     console.log("inside promiseExecutor");
+// }
+
+// var promise1 = new Promise(promiseExecutor);
+
+// promise1.then((somevalue) => {
+//     responsevalue = somevalue
+// })//accepts a callback(passing function a function ) as method
+// console.log(promise1);
+// console.log(responsevalue);
+
+// -----------------------------------------------------------------------------------------
+
+// function promiseExecutor(resolve, reject) {
+//     reject({ response: "responsevalue" });
+//     console.log("inside promiseExecutor");
+// }
+
+// var promise1 = new Promise(promiseExecutor);
+// let responsevalue;
+
+// promise1.catch((somevalue) => {
+//     responsevalue = somevalue;
+// });
+// console.log(responsevalue);//no value as this works fast than promise
+
+//accepts a callback(passing function a function ) as method
+// console.log(promise1);
+// console.log(responsevalue);
+
+
+
+// var promise1 = new Promise(promiseExecutor);
+// let responsevalue;
+
+// promise1.catch((somevalue) => {
+//     responsevalue = somevalue;
+// });
+
+// //accepts a callback(passing function a function ) as method
+// console.log(promise1);
+// setTimeout(() => {
+//     console.log(responsevalue);
+// }, 10000);
+// ---------------------------------------------------------------------------------------------
+
---
+++ SEMfive/VAIBHAV_SIR/08_AUG/fee_31_08/17_custom_promise.js
@@ -0,0 +1,158 @@
+// var promise1 = new Promise(executor)
+
+// function customPromiseExecutor() {
+//     console.log("int custom Promise Executor");
+// }
+
+// function customPromise(executorfuncArg) {
+//     executorfuncArg();
+//     this.state = "pending";
+//     let returnValue = undefined;//does not expose returnValue
+
+// }
+
+// var promiseCustom1 = new customPromise(customPromiseExecutor);
+
+// console.log(promiseCustom1);
+// ------------------------------------------------------------------------
+
+// function customPromiseExecutor(resolve, reject) {
+//     resolve();
+//     console.log("inside promiseExecutor");
+// }
+
+// function customPromise(executorfuncArg) {
+//     this.state = "pending";
+//     executorfuncArg(
+//         () => {
+//             this.state = "fulfilled";
+//             console.log("inside Resolve function");
+//         },
+//         () => {
+//             this.state = "rejected";
+//             console.error("inside Reject function");
+//         },
+//     );//we need to define resolve and reject , they are defined where they are called;
+//     console.log(this.state);
+//     let returnValue = undefined;//does not expose returnValue
+
+// }
+
+// var promiseCustom1 = new customPromise(customPromiseExecutor);
+
+// console.log("promisecustom1=", promiseCustom1);
+// -----------------------------------------------------------------------------------
+// function customPromiseExecutor(resolve, reject) {
+//     setTimeout(() => {
+//         reject("Sample value");
+//         console.log("inside promiseExecutor");
+//     }, 3000);
+// }
+
+// function customPromise(executorfuncArg) {
+//     executorfuncArg(
+//         (anyvalue) => {
+//             this.state = "fulfilled";
+//             console.log("inside Resolve function and anyvalue = ", anyvalue);
+//         },
+//         (anyvalue) => {
+//             this.state = "rejected";
+//             console.error("inside Reject function and anyvalue = ", anyvalue);
+//         },
+//     );//we need to define resolve and reject , they are defined where they are called;
+//     this.state = "pending";
+//     let returnValue = undefined;//does not expose returnValue
+
+// }
+
+// var promiseCustom1 = new customPromise(customPromiseExecutor);
+// console.log("promisecustom1=", promiseCustom1);
+// --------------------------------------------------------------------------------------
+// function customPromiseExecutor(resolve, reject) {
+//     setTimeout(() => {
+//         resolve("Sample value");
+//         console.log("inside promiseExecutor");
+//     }, 5000);
+// }
+
+// function customPromise(executorfuncArg) {
+//     this.state = "pending";
+//     let sucesscallbackcustom;
+//     let errorcallbackcustom;
+//     executorfuncArg(
+//         (sucessresponse) => {
+//             this.state = "fulfilled";
+//             console.log("inside Resolve function and sucessresponse = ", sucessresponse);
+//             sucesscallbackcustom(sucessresponse);
+//         },
+//         (errorresponse) => {
+//             this.state = "rejected";
+//             console.error("inside Reject function and errorresponse = ", errorresponse);
+//             errorcallbackcustom(errorresponse);
+//         },
+//     );//we need to define resolve and reject , they are defined where they are called;
+//     let returnValue = undefined;//does not expose returnValue
+
+//     //this.then use to implement chaining then is functino with also takes a function as an arguments,ie arg1 = fuction ,,, to expose then we create a local arguments
+//     this.then = (arg1callback) => {
+//         sucesscallbackcustom = arg1callback;
+//     }
+// }
+
+// var promiseCustom1 = new customPromise(customPromiseExecutor);
+// console.log("promisecustom1=", promiseCustom1);
+
+// let sucessresponseCustomvalue;
+// promiseCustom1.then((somevalue => {
+//     sucessresponseCustomvalue = somevalue;
+// }));//only works when resolve works
+
+// -------------------------------------------------------------------------------------------------------------
+function customPromiseExecutor(resolve, reject) {
+    setTimeout(() => {
+        reject("Sample value");
+        console.log("inside promiseExecutor");
+    }, 5000);
+}
+
+function customPromise(executorfuncArg) {
+    this.state = "pending";
+    let sucesscallbackcustom;
+    let errorcallbackcustom;
+    executorfuncArg(
+        (sucessresponse) => {
+            this.state = "fulfilled";
+            console.log("inside Resolve function and sucessresponse = ", sucessresponse);
+            sucesscallbackcustom(sucessresponse);
+        },
+        (errorresponse) => {
+            this.state = "rejected";
+            console.error("inside Reject function and errorresponse = ", errorresponse);
+            errorcallbackcustom(errorresponse);
+        },
+    );//we need to define resolve and reject , they are defined where they are called;
+    let returnValue = undefined;//does not expose returnValue
+
+    //this.then use to implement chaining then is functino with also takes a function as an arguments,ie arg1 = fuction ,,, to expose then we create a local arguments
+    this.then = (arg1callback) => {
+        sucesscallbackcustom = arg1callback;
+    }
+
+    this.catch = (arg2callback) => {
+        errorcallbackcustom = arg2callback;
+    }
+}
+
+var promiseCustom1 = new customPromise(customPromiseExecutor);
+console.log("promisecustom1=", promiseCustom1);
+
+// let sucessresponseCustomvalue;
+// promiseCustom1.then((somevalue => {
+//     sucessresponseCustomvalue = somevalue;
+// }));//only works when resolve works
+
+
+let errorresponseCustomvalue;
+promiseCustom1.then((somevalue => {
+    errorresponseCustomvalue = somevalue;
+}));//only works when resolve works

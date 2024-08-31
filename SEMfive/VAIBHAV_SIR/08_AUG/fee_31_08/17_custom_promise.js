80babd62753873b4f08aaeb4647e76f7e490a3b1
Manasvi8274 <Manasvi1238274@gmail.com>
2024-08-31 12:31:01 +0530

    commit

--- SEMfive/VAIBHAV_SIR/08_AUG/fee_31_08/17_custom_promise.js
+++ SEMfive/VAIBHAV_SIR/08_AUG/fee_31_08/17_custom_promise.js
@@ -108,6 +108,57 @@
 // }));//only works when resolve works
 
 // -------------------------------------------------------------------------------------------------------------
+// function customPromiseExecutor(resolve, reject) {
+//     setTimeout(() => {
+//         reject("Sample value");
+//         console.log("inside promiseExecutor");
+//     }, 5000);
+// }
+
+// function customPromise(executorfuncArg) {
+//     this.state = "pending";
+//     let sucesscallbackcustom;
+//     let errorcallbackcustom;
+//     executorfuncArg(
+//         (sucessresponse) => {
+//             this.state = "fulfilled";
+//             console.log("inside Resolve function and sucessresponse = ", sucessresponse);
+//             sucesscallbackcustom(sucessresponse);
+//         },
+//         (errorresponse) => {
+//             this.state = "rejected";
+//             console.error("inside Reject function and errorresponse = ", errorresponse);
+//             errorcallbackcustom(errorresponse);
+//         },
+//     );//we need to define resolve and reject , they are defined where they are called;
+//     let returnValue = undefined;//does not expose returnValue
+
+//     //this.then use to implement chaining then is functino with also takes a function as an arguments,ie arg1 = fuction ,,, to expose then we create a local arguments
+//     this.then = (arg1callback) => {
+//         sucesscallbackcustom = arg1callback;
+//     }
+
+//     this.catch = (arg2callback) => {
+//         errorcallbackcustom = arg2callback;
+//     }
+// }
+
+// var promiseCustom1 = new customPromise(customPromiseExecutor);
+// console.log("promisecustom1=", promiseCustom1);
+
+// // let sucessresponseCustomvalue;
+// // promiseCustom1.then((somevalue => {
+// //     sucessresponseCustomvalue = somevalue;
+// // }));//only works when resolve works
+
+
+// let errorresponseCustomvalue;
+// promiseCustom1.catch((somevalue => {
+//     errorresponseCustomvalue = somevalue;
+// }));//only works when resolve works
+
+// -------------------------------------------------------------------------------------------------------------
+//----------------------------------------------FINAL PROMISE CUSTOM -------------------------------------------
 function customPromiseExecutor(resolve, reject) {
     setTimeout(() => {
         reject("Sample value");
@@ -146,13 +197,13 @@ function customPromise(executorfuncArg) {
 var promiseCustom1 = new customPromise(customPromiseExecutor);
 console.log("promisecustom1=", promiseCustom1);
 
-// let sucessresponseCustomvalue;
-// promiseCustom1.then((somevalue => {
-//     sucessresponseCustomvalue = somevalue;
-// }));//only works when resolve works
+let sucessresponseCustomvalue;
+promiseCustom1.then((somevalue => {
+    sucessresponseCustomvalue = somevalue;
+}));//only works when resolve works
 
 
 let errorresponseCustomvalue;
-promiseCustom1.then((somevalue => {
+promiseCustom1.catch((somevalue => {
     errorresponseCustomvalue = somevalue;
 }));//only works when resolve works

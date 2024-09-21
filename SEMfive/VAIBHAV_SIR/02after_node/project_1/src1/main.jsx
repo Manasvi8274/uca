fa92f3f7b975db924f4350d643f6956897fb2c5a
Manasvi8274 <Manasvi1238274@gmail.com>
2024-09-21 10:51:36 +0530

    commit

---
+++ SEMfive/VAIBHAV_SIR/02after_node/project_1/src1/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client"
+
+const reactProjectRoot = createRoot(document.getElementById("root"));
+
+reactProjectRoot.render(<h2>HELLO WORLD</h2>)
+reactProjectRoot.render(<h2>HELLO WORLD</h2>)

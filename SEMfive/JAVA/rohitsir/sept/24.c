198b1ecec106cf29df0dd81266eb91d56766dc1f
Manasvi8274 <Manasvi1238274@gmail.com>
2024-09-24 20:27:15 +0530

    24 sept

--- SEMfive/JAVA/rohitsir/sept/24.c
+++ SEMfive/JAVA/rohitsir/sept/24.c
@@ -10,21 +10,66 @@
 //     fclose(pt);
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    char input[100]; // Allocate space for the input
+// int main()
+// {
+//     char input[100]; // Allocate space for the input
 
-    printf("Enter a string: ");
-    if (fgets(input, sizeof(input), stdin) != NULL)
-    {
-        printf("You entered: %s", input);
-    }
-    else
-    {
-        printf("An error occurred while reading input.\n");
-    }
+//     printf("Enter a string: ");
+//     if (fgets(input, sizeof(input), stdin) != NULL)
+//     {
+//         printf("You entered: %s", input);
+//     }
+//     else
+//     {
+//         printf("An error occurred while reading input.\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 42;
+//     double pi = 3.14159;
+//     char str[] = "Hello, stdout!";
+
+//     fprintf(stdout, "Integer: %d\n", num);
+//     fprintf(stdout, "Floating-point: %f\n", pi);
+//     fprintf(stdout, "String: %s\n", str);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 42;
+//     double pi = 3.14159;
+//     char str[] = "Hello, stdout!";
+//     FILE *ptr;
+//     ptr = fopen("./a.txt","w");
+//     fprintf(ptr, "Integer: %d\n", num);
+//     fprintf(ptr, "Floating-point: %f\n", pi);
+//     fprintf(ptr, "String: %s\n", str);
+//     fclose(ptr);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     FILE *pt = fopen("./a.txt", "r");
+//     char st;
+//     for (int i = 0; i < 100; i++)
+//     {
+//         // st = fgetc(pt);
+//         fprintf(stdout, "%c", fgetc(pt));
+//     }
+//     fclose(pt);
+// }
+

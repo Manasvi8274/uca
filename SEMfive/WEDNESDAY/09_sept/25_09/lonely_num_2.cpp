e7c949bbdf032282c67d528c81e4a6997138b0c7
Manasvi8274 <Manasvi1238274@gmail.com>
2024-09-25 19:21:32 +0530

    commit

---
+++ SEMfive/WEDNESDAY/09_sept/25_09/lonely_num_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n;
+    // int arr[n];
+    // for(int i = 0 ;i < n ;i++){
+    //     cin>>arr[i];
+    // }
+
+    // map<int,int> mp;
+}

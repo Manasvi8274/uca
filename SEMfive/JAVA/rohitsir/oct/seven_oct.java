f0af9a7f51016bfd489e25b6952195b594935045
manasvi8274 <manasvi1238274@gmail.com>
2024-10-09 19:59:35 +0530

    commit

--- SEMfive/JAVA/rohitsir/oct/seven_oct.java
+++ SEMfive/JAVA/rohitsir/oct/seven_oct.java
@@ -1,6 +1,46 @@
 
 public class seven_oct {
+    int x;
+    int airbags;
+    seven_oct(){
+        
+    }
+
+    seven_oct(int x,int airbags){//multiargument constructor
+        this.x = x;
+        this.airbags = airbags;
+    }
+
+    seven_oct(seven_oct copy){
+        this.x = copy.x;
+        this.airbags = copy.airbags;
+    }
+
     public static void main(String[] args){
-        System.out.println(System.out);
+        // seven_oct one = new seven_oct();
+        // seven_oct two = new seven_oct(20, 30);
+        // seven_oct three = new seven_oct(two);
+        // System.out.println(one+" "  + one.x);
+        // int a;
+        // System.out.println(a);
+
+        UCA.main1();
+    }
+}
+
+class UCA<K, V>{
+    K key;
+    V value;
+    UCA(K key,V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public static void main1(){
+        UCA<String,String> uca1 = new UCA<String,String>("Hello" , "WORLD");
+        UCA<Integer,Integer> uca2 = new UCA<Integer,Integer>(1 , 2);
+        System.out.println(uca1.key + " " + uca1.value);
+        System.out.println(uca2.key + " " + uca2.value);
+        return;
     }
 }
---
+++ xtra/one_q.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class one_q{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            int s;
+            s=sc.nextInt();
+            arr[i] = s;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] +"\t");
+        }
+        System.out.println();
+        //sol
+        for(int i = 0;i < n;i++){
+            if (arr[i] >=n) arr[i] += (arr[arr[i]]) * n;
+            else    arr[i] += (arr[arr[i]] % n) * n;
+        }
+        for(int i=0;i<n;i++){
+            arr[i] = arr[i] / n;
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] +"\t");
+        }
+    }
+}

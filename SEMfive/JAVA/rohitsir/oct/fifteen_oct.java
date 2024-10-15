75bde3815b666295095ac125b5714b59b875d05d
manasvi8274 <manasvi1238274@gmail.com>
2024-10-15 19:51:05 +0530

    cmt

---
+++ SEMfive/JAVA/rohitsir/oct/fifteen_oct.java
@@ -0,0 +1,27 @@
+package semfive.java.rohitsir.oct;
+
+// interface calculator{
+//     public int add(int a,int b);
+//     public int sub(int a,int b);
+
+// }
+
+// public class fifteen_oct implements calculator
+// {
+//     public static void main(String[] args){
+//         System.out.println("hello23e");
+//     }
+// }
+interface List<K>{
+    public int add(K a);
+    public int remove(K a);
+    public boolean isEmpty();
+    public int getSize();
+}
+
+public class fifteen_oct implements List<>
+{
+    public static void main(String[] args){
+        System.out.println("hello23e");
+    }
+}

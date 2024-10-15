1fa200fe8aad029d03c952a8177fe5dae8452b30
manasvi8274 <manasvi1238274@gmail.com>
2024-10-15 20:19:23 +0530

    interface

--- SEMfive/JAVA/rohitsir/oct/fifteen_oct.java
+++ SEMfive/JAVA/rohitsir/oct/fifteen_oct.java
@@ -1,9 +1,78 @@
 package semfive.java.rohitsir.oct;
 
-// interface calculator{
-//     public int add(int a,int b);
-//     public int sub(int a,int b);
+// interface A{
+//     int add(int a,int b);
+// }
 
+// interface B{
+//     int add(int a,int b);
+// }
+
+// public class fifteen_oct implements A,B{
+//     public int add(int a,int b){
+//         return a+b;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println("hello");
+
+//     }
+// }
+
+// class A
+// {
+//     public int val;
+//     A(int def){
+//         val = def;
+//     }
+// }
+
+// public class fifteen_oct extends A{
+//     fifteen_oct(int val){
+//         super(val);
+//     }
+//     public static void main(String[] args) {
+//         fifteen_oct f = new fifteen_oct(100);
+//         System.out.println(f.val);
+//     }
+// }
+
+// class A
+// {
+//     public int val;
+//     A(int def){
+//         val = def;
+//     }
+// }
+
+// public class fifteen_oct extends A{
+//     public static void main(String[] args) {
+//         fifteen_oct f = new fifteen_oct();
+//         System.out.println(f.val);
+//     }
+// }
+
+// public class fifteen_oct
+// {
+//     public static void main(String[] args){
+//         List<Integer> array = new ArrayList<>();
+//         List<Integer> linkedlist = new LinkedList<>();
+
+//         array.add(1);
+//         array.add(2);
+//         array.add(3);
+
+//         linkedlist.add(1);
+//         linkedlist.add(2);
+//         linkedlist.add(3);
+
+//         System.out.println("Array : " + array);
+//         System.out.println("linkedlist : " + linkedlist);
+//     }
+// }
+
+// interface calculator{
+//     public itn add(int a,int b);
+//     public int sub(int a,int b);
 // }
 
 // public class fifteen_oct implements calculator
@@ -12,16 +81,40 @@ package semfive.java.rohitsir.oct;
 //         System.out.println("hello23e");
 //     }
 // }
-interface List<K>{
-    public int add(K a);
-    public int remove(K a);
-    public boolean isEmpty();
-    public int getSize();
-}
+// interface List<K>{
+//     public int add(K a);
+//     public int remove(K a);
+//     public boolean isEmpty();
+//     public int getSize();
+// }
 
-public class fifteen_oct implements List<>
-{
-    public static void main(String[] args){
-        System.out.println("hello23e");
-    }
-}
+// class Array implements List<K>{
+//     public int add(K a){
+//         System.out.println("ARRAY");
+//     }
+//     public int remove(K a){
+//         System.out.println("ARRAY");
+//     }
+//     public boolean isEmpty(){
+//         System.out.println("ARRAY");
+//     }
+//     public int getSize(){
+//         System.out.println("ARRAY");
+//     }
+// }
+
+// class Linkedlist implements List<K>{
+//     public int add(K a){
+//         System.out.println("LL");
+//     }
+//     public int remove(K a){
+//         System.out.println("LL");
+//     }
+//     public boolean isEmpty(){
+//         System.out.println("LL");
+//     }
+//     public int getSize(){
+//         System.out.println("LL");
+//     }
+// }
+

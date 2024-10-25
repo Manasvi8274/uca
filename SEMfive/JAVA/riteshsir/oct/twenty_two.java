3fd13cb2684789e738c97f52b7f8d24a643d2be5
manasvi8274 <manasvi1238274@gmail.com>
2024-10-25 21:53:54 +0530

    25

---
+++ SEMfive/JAVA/riteshsir/oct/twenty_two.java
@@ -0,0 +1,35 @@
+package semfive.java.riteshsir.oct;
+
+// class car{
+//     protected  void m1(){
+//         System.out.println("car1 m1");
+//     }
+//     protected void m3(){
+//         System.out.println("car m3");
+//     }
+// }
+
+// class maruti extends car{
+//     private void m1(){
+//         System.out.println("Maruti m1");
+//     }
+//     public void m2(){
+//         System.out.println("Maruti m2");
+//     }
+//     public void m3(){
+//         System.out.println("maruti1 m3");
+//     }
+// }
+
+
+// public class twenty_two {
+//     public static void main(String[] args){
+//         car c = new maruti();
+//         c.m1();
+//     }
+// }
+
+
+class twenty_two{
+
+}
--- zjava/lec_11_strings/assignment.java
+++ zjava/lec_11_strings/assignment.java
@@ -19,7 +19,94 @@ public class assignment{
         System.out.println(flag);
     }
 
+    public static void remove_consecutive(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String ans = "";
+        ans+=str.charAt(0);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i) != str.charAt(i-1)){
+                ans+=str.charAt(i);
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void reverse_each_word(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ans = "";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == ' ' || i == str.length()-1){
+                int j;
+                if(i == str.length()-1){
+                    j=i;
+                }
+                else{
+                    j=i-1;
+                }
+                while(j>=0 && str.charAt(j) !=' '){
+                    ans+=str.charAt(j);
+                    j--;
+                }
+                if(i != str.length()-1)    ans+=' ';
+            }
+        }
+        
+
+        System.out.println(ans);
+    }
+
+    public static void stringcompres() {
+        Scanner sc = new  Scanner(System.in);
+        String s = sc.next();
+        String ans = "";
+        int c=1;
+        for(int i=0;i<s.length();i++){
+            if(i == 0){
+                ans+=s.charAt(i);
+            }
+            else if (i == s.length()-1) {
+                if(c==1){
+                    ans+=s.charAt(i);
+                }
+                if(s.charAt(i) == s.charAt(i-1)){
+                    ans+=String.valueOf(c+1);
+                }
+                if(c!=1 && s.charAt(i) != s.charAt(i-1)){
+                    ans+=String.valueOf(c);
+                    ans+=s.charAt(i);
+                }
+            }
+            else if(s.charAt(i) == s.charAt(i-1)){
+                c++;
+            }
+            else{
+                if(c!=1)    ans+=String.valueOf(c);
+                // System.out.println(ans + " " + c);
+                c=1;
+                ans+=s.charAt(i);
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void count_word(){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int ans=1;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ' ')  ans++;
+        }
+        System.out.println(ans);
+    }
+
+
     public static void main(String[] args) {
-        check_perm();
+        // check_perm();
+        // remove_consecutive();
+        // reverse_each_word();
+        // stringcompres();
+        count_word();
     }
 }
